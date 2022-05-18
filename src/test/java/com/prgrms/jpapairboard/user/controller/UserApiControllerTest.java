3ed7b464e693ca8c441b9fa7c3e000821494055b
ecvheo1 <ecvheo@naver.com>
2022-05-19 00:13:31 +0900

    :recycle: refact(UserApiControllerTest): Method Naming 수정 및 가독성을 위한 메소드 분리 (by 류영준, 유도진)

--- src/test/java/com/prgrms/jpapairboard/user/controller/UserApiControllerTest.java
+++ src/test/java/com/prgrms/jpapairboard/user/controller/UserApiControllerTest.java
@@ -1,27 +1,30 @@
 package com.prgrms.jpapairboard.user.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.prgrms.jpapairboard.user.entity.User;
 import com.prgrms.jpapairboard.user.service.UserService;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserApiController.class)
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class UserApiControllerTest {
 
     @Autowired
@@ -34,82 +37,91 @@ class UserApiControllerTest {
     UserService userService;
 
     @Nested
-    @DisplayName("addUser")
-    class Describe_addUser {
+    class Method_save_Test {
 
         private static final String URL = "/api/v1/users";
 
         @Nested
-        @DisplayName("필수 값이 모두 존재하면")
-        class Context_with_required_data_exist {
+        class 필수_값이_모두_존재하는_입력이_들어오면 {
 
             @Test
-            @DisplayName("create 를 응답한다")
-            void It_response_create() throws Exception {
+            void Created_200을_응답한다() throws Exception {
                 final Map<String, String> requestMap = new HashMap<>();
                 requestMap.put("email", "san9580@naver.com");
                 requestMap.put("name", "san kim");
-                final var content = objectMapper.writeValueAsString(requestMap);
 
-                final var request = MockMvcRequestBuilders.post(URL)
-                        .content(content)
-                        .contentType(MediaType.APPLICATION_JSON);
+                final var result = mockMvc.perform(post(URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(toJson(requestMap)));
 
-                final var result = mockMvc.perform(request);
-                verify(userService, times(1)).saveUser(any(User.class));
+                verify(userService, times(1)).save(any(User.class));
                 result.andExpect(status().isCreated());
+                assertThat(result.andReturn().getResponse()).isNotNull();
             }
         }
 
         @Nested
-        @DisplayName("모든 값이 존재하면")
-        class Context_with_all_data_exist {
+        class 모든_값이_존재하는_입력이_들어오면 {
 
             @Test
-            @DisplayName("create 를 응답한다")
-            void It_response_create() throws Exception {
+            void create_200을_응답한다() throws Exception {
                 final Map<String, Object> requestMap = new HashMap<>();
                 requestMap.put("email", "san9580@naver.com");
                 requestMap.put("name", "san kim");
                 requestMap.put("age", 29);
                 requestMap.put("hobby", "soccer");
-                final var content = objectMapper.writeValueAsString(requestMap);
 
-                final var request = MockMvcRequestBuilders.post(URL)
-                        .content(content)
-                        .contentType(MediaType.APPLICATION_JSON);
+                final var result = mockMvc.perform(post(URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(toJson(requestMap)));
 
-                final var result = mockMvc.perform(request);
-                verify(userService, times(1)).saveUser(any(User.class));
                 result.andExpect(status().isCreated());
+                assertThat(result.andReturn().getResponse()).isNotNull();
             }
         }
 
         @Nested
-        @DisplayName("name 이 존재하지 않으면")
-        class Context_with_name_null {
+        class 잘못된_name을_포함한_입력이_들어오면 {
 
-            @Test
-            @DisplayName("BadRequest 를 응답한다")
-            void It_response_BadRequest() {
+            @ParameterizedTest
+            @NullAndEmptySource
+            void BadRequest_400을_응답한다(String inputName) throws Exception {
+                final Map<String, Object> requestMap = new HashMap<>();
+                requestMap.put("email", "san9580@naver.com");
+                requestMap.put("name", inputName);
+                requestMap.put("age", 29);
+                requestMap.put("hobby", "soccer");
 
+                final var result = mockMvc.perform(post(URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(toJson(requestMap)));
+
+                result.andExpect(status().isBadRequest());
             }
         }
 
         @Nested
-        @DisplayName("email 이 존재하지 않으면")
-        class Context_with_email_null {
+        class email이_누락된_입력이_들어오면 {
 
-            @Test
-            @DisplayName("BadRequest 를 응답한다")
-            void It_response_BadRequest() {
+            @ParameterizedTest
+            @NullAndEmptySource
+            void BadRequest_400을_응답한다(String inputEmail) throws Exception {
+                final Map<String, Object> requestMap = new HashMap<>();
+                requestMap.put("email", inputEmail);
+                requestMap.put("name", "san");
+                requestMap.put("age", 20);
+                requestMap.put("hobby", "soccer");
 
+                final var result = mockMvc.perform(post(URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(toJson(requestMap)));
+
+                result.andExpect(status().isBadRequest());
             }
         }
 
         @Nested
-        @DisplayName("email 이 유효하지 않은 형태면")
-        class Context_with_email_invalid {
+        class 유요하지_하지_않는_email을_포함한_입력이_들어오면 {
 
             @Test
             @DisplayName("BadRequest 를 응답한다")
@@ -127,6 +139,10 @@ class UserApiControllerTest {
             void It_response_BadRequest() {
 
             }
+        }
+
+        private String toJson(Object obj) throws JsonProcessingException {
+            return objectMapper.writeValueAsString(obj);
         }
     }
 }
